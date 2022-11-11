226814d926fc15356c4016fa7e204a684a0d24b0
zmb <19725912@qq.com>
2022-11-11 16:32:59 +0800

    revised in office

--- src/pages/progress/disassembly.vue
+++ src/pages/progress/disassembly.vue
@@ -1,11 +1,44 @@
 <template>
-这是disassembly.vue，项目拆解
+  <a-tree class="tree" :tree-data="treeData" v-model:selectedKeys="selectedKeys">
+  </a-tree>
 </template>
 
 <script setup lang="ts">
+import {TreeProps} from "ant-design-vue";
+import {ref, watch} from "vue";
 
+const treeData: TreeProps['treeData'] = [
+  {
+    title: '我是a',
+    key: 'a',
+    children: [
+      {
+        title: '我是a1',
+        key: 'a1'
+      },
+      {
+        title: '我是a22',
+        key: 'a2'
+      }
+    ]
+  },
+  {
+    title: '我是b',
+    key: 'b'
+  }
+]
+
+const selectedKeys = ref<string[]>([]);
+
+watch(selectedKeys, () => {
+  console.log(selectedKeys.value[0]);
+});
 </script>
 
 <style scoped lang="scss">
-
+:deep(.tree) {
+  background-color: white;
+  width: 200px;
+  padding: 10px;
+}
 </style>
